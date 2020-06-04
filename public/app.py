ad26f2b84634e956c70c5e8743d5b5b3aed0884b
dantsub <dany.mrtinez@gmail.com>
2020-06-04 13:03:08 -0500

    [update] create new route for application

--- public/app.py
+++ public/app.py
@@ -3,11 +3,12 @@
 '''
 from flask import Flask, render_template, flash, redirect, request, url_for,\
     jsonify, abort
-from datetime import datetime
 from uuid import uuid4
 from models.user import User
 from models.role import Role
+from public.forms import SignUp, LogIn
 import json
+
 
 app = Flask(__name__)
 app.config.from_object('public.config')
@@ -15,10 +16,11 @@ app.config.from_object('public.config')
 
 @app.route('/')
 def index():
-   ''' render index.html '''
-   cache = str(uuid4())
-   emails = [email['email'] for email in User.readAll().values()]
-   return render_template('index.html', cache=cache, emails=emails)
+    ''' render index.html '''
+    cache = str(uuid4())
+    signUp = SignUp(request.form)
+    logIn = LogIn(request.form)
+    return render_template('index.html', cache=cache, signUp=signUp, logIn=logIn)
 
 
 @app.route('/sign_up', methods=['POST'])
@@ -52,6 +54,60 @@ def email_validation():
     if len(data) == 0:
         emails = [email['email'] for email in User.readAll().values()]
         return jsonify(emails)
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def method_name():
+    print(request.form)
+    form = SignUp(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('test.html', form=form)
+    return render_template('test_signUp.html', form=form)
+
+
+@app.route('/reports', methods=['GET', 'POST'])
+def reports():
+    signUp = SignUp(request.form)
+    logIn = LogIn(request.form)
+    cache = str(uuid4())
+    return render_template('reports.html', signUp=signUp, logIn=logIn, cache=cache)
+
+
+@app.route('/map', methods=['GET', 'POST'])
+def map():
+    signUp = SignUp(request.form)
+    logIn = LogIn(request.form)
+    cache = str(uuid4())
+    return render_template('map.html', signUp=signUp, logIn=logIn, cache=cache)
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    signUp = SignUp(request.form)
+    logIn = LogIn(request.form)
+    cache = str(uuid4())
+    return render_template('about.html', signUp=signUp, logIn=logIn, cache=cache)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    signUp = SignUp(request.form)
+    logIn = LogIn(request.form)
+    cache = str(uuid4())
+    return render_template('contact.html', signUp=signUp, logIn=logIn, cache=cache)
+
+
+@app.route('/help', methods=['GET', 'POST'])
+def help():
+    signUp = SignUp(request.form)
+    logIn = LogIn(request.form)
+    cache = str(uuid4())
+    return render_template('help.html', signUp=signUp, logIn=logIn, cache=cache)
+
+
+@app.route('/help_form', methods=['GET', 'POST'])
+def help_form():
+   pass
 
 
 @app.errorhandler(404)
